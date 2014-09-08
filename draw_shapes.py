717acd3489093e5dcbcabfcd1ed58974ecac2d03
lois <lois@aims.edu.gh>
2014-09-08 08:58:56 +0000

    more comments

--- draw_shapes.py
+++ draw_shapes.py
@@ -2,9 +2,9 @@ __author__ = 'lois'
 import turtle
 import geom_formulae
 
-def draw_rectangle(l,b):
-    turtle.forward(l)
-    turtle.left(90)
+def draw_rectangle(l,b):#drawing a rectangle
+    turtle.forward(l) #length of rectangle
+    turtle.left(90) #angle of corner of rectangle
     turtle.forward(b)
     turtle.left(90)
     turtle.forward(l)
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
 
 
-def draw_triangle(b,h):
+def draw_triangle(b,h):#drawing a triangle.
     turtle.forward(b)
     turtle.left(120)
     turtle.forward(h)
@@ -33,7 +33,7 @@ if __name__ == "__main__":
     draw_triangle(400, 400)
 
 
-def draw_parallelogram(sideA,sideB):
+def draw_parallelogram(sideA,sideB):#drawing a parallelogram.
     turtle.forward(sideA)
     turtle.left(60)
     turtle.forward(sideB)
@@ -49,8 +49,8 @@ if __name__ == "__main__":
     draw_parallelogram(300, 200)
 
 
-def draw_circle(pi,r):
-    turtle.circle(150)
+def draw_circle(pi,r):  # drawing a circle
+    turtle.circle(150) #radus of circle
     turtle.done()
 
 if __name__ == "__main__":
