ddeb0ffc3c1667fab951b54c3be5a99ba3572ddb
lois <lois@aims.edu.gh>
2014-09-24 01:26:35 +0000

    more comments

--- integration.py
+++ integration.py
@@ -1,21 +1,17 @@
 __author__ = 'lois'
 import numpy as np
 
-def simps_method(func_, l_range, u_range, c): # c is the count
-
-    h = (u_range - l_range)/c
-    a = func_(l_range) + func_(u_range)
-    for i in range(1, c, 2):
-        a += 4*func_(l_range+i * h)
-    for i in range(2, c-1, 2):
-        a += 2*func_(l_range + i * h)
-        return a*h/3
-
+def simpson(f, a, b, n):
+    delta = (b - a)/n
+    x = np.linspace(a, b, n + 1)
+    s= 0
+    for i in range(0, n, 2):
+        s += f(x[i]) + 4*f(x[i + 1]) + f(x[i+2])
+    return (delta/3)*s
 if __name__ == "__main__":
-    print(simps_method(lambda x: (x**2-1), -2, 2, 10))
-    print(simps_method(lambda x: (x**2-1), -2, 2, 10000))
-    print(simps_method(lambda x: (1/x), 0.1, 2, 100))
-
+    print("For Simpson, Integral of x^2-1 from -2 to 2 is: ",simpson(lambda x: (x**2 - 1), -2, 2, 10))
+    print("For Simpson, Integral of x^2-1 from -2 to 2 is: ",simpson(lambda x: (x**2-1), -2, 2, 10000))
+    print("For Simpson, Integral of x^2-1 from -2 to 2 is: ",simpson(lambda x: (1/x), 0.1, 2, 100))
 
 # the midpoint method  is shown below
 
--- secant.py
+++ secant.py
@@ -2,7 +2,7 @@ __author__ = 'lois'
 
 from numpy import *
 from math import*
-
+"""
 def sec_method (f, a0 ,b0 , error):
 
     x0=a0
@@ -24,3 +24,17 @@ if __name__ == "__main__":
     print(sec_method(lambda x: sin(x)+cos(x), pi/2, 3*(pi/2), 1e-6))
     print(sec_method(lambda x: sin(x)+cos(x), -pi/2, pi/2, 1e-6))
     print(sec_method(lambda x: (x**3), 5, 6, 1e-9))
+"""
+def secant(f, g1, g2, err):
+    x0 = g1
+    x1 = g2
+    root = x1
+    while fabs(f(x1)) > err and f(x0) != f(x1):
+         x2 = (x0*f(x1) - x1*f(x0))/(f(x1) - f(x0))
+         x0 = x1
+         x1 = x2
+         root = x2
+    return root
+print("The root of sin(x) + cos(x): ", secant(lambda x: (sin(x) + cos(x)), pi/2, 3*pi/2, 1e-6))
+print("The root of sin(x) + cos(x): ", secant(lambda x: sin(x)+cos(x), -pi/2, pi/2, 1e-6))
+print("The root of sin(x) + cos(x): ", secant(lambda x: (x**3), 5, 6, 1e-9))
